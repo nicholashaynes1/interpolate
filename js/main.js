38644d49effd1f631cc2652b0ff4856e7d37335c
nicholashaynes1 <nicholashaynes1@gmail.com>
2018-02-09 16:24:12 -0700

    When every intepolated text is revealed the next button shows.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
 var revealText = document.getElementsByClassName('interpolateText');
-var text= document.getElementsByClassName('interpolateText');
-// iteratior
+//an array used to hold all the interpolate text objects
+var revealArray = [];
+var nextButton = document.getElementsByClassName('nextButton');
 
 // This loops through all my interpolated text boxes
 for(i=0; i<revealText.length; i++)
 {
 	revealText[i].setAttribute('onClick', 'reveal(this)');
+	//adding the interpolate objects to the array
+	revealArray.push(revealText[i]);
 };
 
 // The function used change the opacity on click @me is the variable that allows for the clicked p to be the revealed one.
 function reveal(me)
 {
 	me.style.opacity = "1";
+	// This checks to see if every interpolated text is revealed and if it is to show the next level button.
+	if(revealArray.every(checkOpacity) == true)
+	{
+		//Make the next level buton visible
+		nextButton[0].style.opacity = '1';
+		revealText.classList.add('nextPage');
+	}
+}
+
+//Checks the opactiy of every button
+function checkOpacity(me)
+{
+	return me.style.opacity == true;
 }
